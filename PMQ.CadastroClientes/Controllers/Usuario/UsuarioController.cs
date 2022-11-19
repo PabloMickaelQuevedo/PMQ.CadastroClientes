b81aa5e643ceeb767b1806b703928d8a4a7a08b5
PabloMickaelQuevedo <drag-rox@hotmail.com>
2022-11-19 15:55:31 -0300

    Adicionado a Model usuario no projeto

---
+++ PMQ.CadastroClientes/Controllers/Usuario/UsuarioController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PMQ.CadastroClientes.Api.Controllers.Usuario
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+    }
+}
---
+++ PMQ.CadastroClientes/Models/Usuario/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PMQ.CadastroClientes.Api.Models.Usuario
+{
+    public class UsuarioModel
+    {
+        public string Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Documento { get; set; }
+        public string? Email { get; set; }
+        public string? Telefone { get; set; }
+        public string? Endereco { get; set; }
+    }
+}
