163116a03c7f9979dd41e381eb52188a86adf37a
PabloMickaelQuevedo <drag-rox@hotmail.com>
2022-11-20 14:23:17 -0300

    Adicionado as Tasks das interfaces no Repositories para os metodos que vão ser utilizado no CRUD.

---
+++ PMQ.CadastroClientes/Infra/Data/Repositories/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using PMQ.CadastroClientes.Api.Models.Usuario;
+
+namespace PMQ.CadastroClientes.Api.Infra.Data.Repositories.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<UsuarioModel> BuscarPorId(string id);
+        Task<UsuarioModel> AdicionarUsuario(UsuarioModel usuario);
+        Task<UsuarioModel> AtualizarUsuario(UsuarioModel usuario, string id);
+        Task<UsuarioModel> DeletarUsuario(string id);
+    }
+}
