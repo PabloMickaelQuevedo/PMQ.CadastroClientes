1f3a5758930bd11c0d1cb7e5c6b259af89359b96
PabloMickaelQuevedo <drag-rox@hotmail.com>
2022-11-20 19:12:12 -0300

    Adicionado a pasta Mapper, e mapeado as tabelas na classe UsuarioMapper

---
+++ PMQ.CadastroClientes/Infra/Data/Mapper/UsuarioMappercs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PMQ.CadastroClientes.Api.Models.Usuario;
+
+namespace PMQ.CadastroClientes.Api.Infra.Data.Mapper
+{
+    public class UsuarioMappercs : IEntityTypeConfiguration<UsuarioModel>
+    {
+        public void Configure(EntityTypeBuilder<UsuarioModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.Documento).IsRequired().HasMaxLength(150);
+            builder.Property(x => x.Email).HasMaxLength(150);
+            builder.Property(x => x.Telefone).IsRequired().HasMaxLength(150);
+            builder.Property(x => x.Endereco).HasMaxLength(150);
+        }
+    }
+}
